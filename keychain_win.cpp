8b1a574f354728c1194164aec318fd786110f109
Frank Osterfeld <osterfeld@kde.org>
2012-05-05 10:37:48 +0200

    Port Windows impl to the job API.

--- keychain_win.cpp
+++ keychain_win.cpp
@@ -17,21 +17,15 @@
 
 using namespace QKeychain;
 
-QtKeychain::Error Keychain::Private::readEntryImpl( QByteArray* pw,
-                                                  const QString& key,
-                                                  QString* err ) {
-    Q_ASSERT( pw );
-    Q_ASSERT( err );
-    err->clear();
-
+void ReadPasswordJob::Private::doStart() {
     //Use settings member if there, create local settings object if not
-    std::auto_ptr<QSettings> local( !settings ? new QSettings( service ) : 0 );
-    QSettings* actual = settings ? settings.data() : local.get();
+    std::auto_ptr<QSettings> local( !q->settings() ? new QSettings( q->service() ) : 0 );
+    QSettings* actual = q->settings() ? q->settings() : local.get();
 
     QByteArray encrypted = actual->value( key ).toByteArray();
     if ( encrypted.isNull() ) {
-        *err = tr("Entry not found");
-        return EntryNotFound;
+        q->emitFinishedWithError( EntryNotFound, tr("Entry not found") );
+        return;
     }
 
     DATA_BLOB blob_in, blob_out;
@@ -40,28 +34,43 @@ QtKeychain::Error Keychain::Private::readEntryImpl( QByteArray* pw,
     blob_in.cbData = encrypted.size();
 
     const BOOL ret = CryptUnprotectData( &blob_in,
-                                         NULL,
+                                        NULL,
                                          NULL,
                                          NULL,
                                          NULL,
                                          0,
                                          &blob_out );
     if ( !ret ) {
-        *err = tr("Could not decrypt data");
-        return OtherError;
+        q->emitFinishedWithError( OtherError, tr("Could not decrypt data") );
+        return;
     }
-    *pw = QByteArray( reinterpret_cast<char*>( blob_out.pbData ), blob_out.cbData );
+
+    data = QByteArray( reinterpret_cast<char*>( blob_out.pbData ), blob_out.cbData );
     SecureZeroMemory( blob_out.pbData, blob_out.cbData );
     LocalFree( blob_out.pbData );
-    return NoError;
+
+    q->emitFinished();
 }
 
-QtKeychain::Error Keychain::Private::writeEntryImpl( const QString& key,
-                                                   const QByteArray& data_,
-                                                   QString* err ) {
-    Q_ASSERT( err );
-    err->clear();
-    QByteArray data = data_;
+void WritePasswordJob::Private::doStart() {
+    if ( mode == Delete ) {
+        //Use settings member if there, create local settings object if not
+        std::auto_ptr<QSettings> local( !q->settings() ? new QSettings( q->service() ) : 0 );
+        QSettings* actual = q->settings() ? q->settings() : local.get();
+        actual->remove( key );
+        actual->sync();
+        if ( actual->status() != QSettings::NoError ) {
+            const QString err = actual->status() == QSettings::AccessError
+                    ? tr("Could not delete encrypted data from settings: access error")
+                    : tr("Could not delete encrypted data from settings: format error");
+            q->emitFinishedWithError( OtherError, err );
+        } else {
+            q->emitFinished();
+        }
+        return;
+    }
+
+    QByteArray data = mode == Binary ? binaryData : textData.toUtf8();
     DATA_BLOB blob_in, blob_out;
     blob_in.pbData = reinterpret_cast<BYTE*>( data.data() );
     blob_in.cbData = data.size();
@@ -73,64 +82,26 @@ QtKeychain::Error Keychain::Private::writeEntryImpl( const QString& key,
                                        0,
                                        &blob_out );
     if ( !res ) {
-        *err = tr("Encryption failed"); //TODO more details available?
-        return OtherError;
+        q->emitFinishedWithError( OtherError, tr("Encryption failed") ); //TODO more details available?
+        return;
     }
 
     const QByteArray encrypted( reinterpret_cast<char*>( blob_out.pbData ), blob_out.cbData );
     LocalFree( blob_out.pbData );
 
     //Use settings member if there, create local settings object if not
-    std::auto_ptr<QSettings> local( !settings ? new QSettings( service ) : 0 );
-    QSettings* actual = settings ? settings.data() : local.get();
+    std::auto_ptr<QSettings> local( !q->settings() ? new QSettings( q->service() ) : 0 );
+    QSettings* actual = q->settings() ? q->settings() : local.get();
     actual->setValue( key, encrypted );
     actual->sync();
     if ( actual->status() != QSettings::NoError ) {
-        *err = actual->status() == QSettings::AccessError
+
+        const QString errorString = actual->status() == QSettings::AccessError
                 ? tr("Could not store encrypted data in settings: access error")
                 : tr("Could not store encrypted data in settings: format error");
-        return OtherError;
+        q->emitFinishedWithError( OtherError, errorString );
+        return;
     }
 
-    return NoError;
-}
-
-QtKeychain::Error Keychain::Private::deleteEntryImpl( const QString& key,
-                                                    QString* err ) {
-    Q_ASSERT( err );
-    err->clear();
-    std::auto_ptr<QSettings> local( !settings ? new QSettings( service ) : 0 );
-    QSettings* actual = settings ? settings.data() : local.get();
-    actual->remove( key );
-    actual->sync();
-    if ( actual->status() != QSettings::NoError ) {
-        *err = actual->status() == QSettings::AccessError
-                ? tr("Could not delete encrypted data from settings: access error")
-                : tr("Could not delete encrypted data from settings: format error");
-        return OtherError;
-    }
-
-    return NoError;
-}
-
-
-QtKeychain::Error Keychain::Private::entryExistsImpl( bool* exists,
-                                                    const QString& key,
-                                                    QString* err ) {
-    Q_ASSERT( exists );
-    Q_ASSERT( err );
-    err->clear();
-    *exists = false;
-    std::auto_ptr<QSettings> local( !settings ? new QSettings( service ) : 0 );
-    QSettings* actual = settings ? settings.data() : local.get();
-    const bool ex = actual->contains( key );
-    if ( actual->status() != QSettings::NoError ) {
-        *err = actual->status() == QSettings::AccessError
-                ? tr("Could not read settings: access error")
-                : tr("Could not read settings: format error");
-        return OtherError;
-    }
-
-    *exists = ex;
-    return NoError;
+    q->emitFinished();
 }
