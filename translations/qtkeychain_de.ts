51a0400eb9ecb5d8f6bb8e68dce77ef52bcc18bf
Daniel Molkentin <daniel@molkentin.de>
2013-02-06 17:05:36 +0100

    Add German translation

---
+++ translations/qtkeychain_de.ts
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>QKeychain::ReadPasswordJobPrivate</name>
+    <message>
+        <location filename="../keychain_dbus.cpp" line="47"/>
+        <source>No keychain service available</source>
+        <translation>Kein Schlüsselbund-Dienst verfügbar</translation>
+    </message>
+    <message>
+        <location filename="../keychain_dbus.cpp" line="49"/>
+        <source>Could not open wallet: %1; %2</source>
+        <translation>Konnte Brieftasche nicht öffnen: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../keychain_dbus.cpp" line="85"/>
+        <source>Access to keychain denied</source>
+        <translation>Zugriff auf Schlüsselbund verweigert</translation>
+    </message>
+    <message>
+        <location filename="../keychain_dbus.cpp" line="98"/>
+        <source>Could not determine data type: %1; %2</source>
+        <translation>Datentyp kann nicht ermittelt werden: %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../keychain_dbus.cpp" line="117"/>
+        <source>Could not read password: %1; %2</source>
+        <translation>Passwort konnte nicht ausgelesen werden: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../keychain_mac.cpp" line="74"/>
+        <source>Password not found</source>
+        <translation>Passwort nicht gefunden</translation>
+    </message>
+    <message>
+        <location filename="../keychain_win.cpp" line="27"/>
+        <source>Entry not found</source>
+        <translation>Eintrag nicht gefunden</translation>
+    </message>
+    <message>
+        <location filename="../keychain_win.cpp" line="44"/>
+        <source>Could not decrypt data</source>
+        <translation>Kann Daten nicht entschlüsseln</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::WritePasswordJobPrivate</name>
+    <message>
+        <location filename="../keychain_dbus.cpp" line="165"/>
+        <location filename="../keychain_dbus.cpp" line="202"/>
+        <source>Could not open wallet: %1; %2</source>
+        <translation>Konnte Brieftasche nicht öffnen: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../keychain_dbus.cpp" line="180"/>
+        <source>Access to keychain denied</source>
+        <translation>Zugriff auf Schlüsselbund verweigert</translation>
+    </message>
+    <message>
+        <location filename="../keychain_win.cpp" line="64"/>
+        <source>Could not delete encrypted data from settings: access error</source>
+        <translation>Kann verschlüsselte Daten nicht aus den Einstellungen entfernen: Zugriffsfehler</translation>
+    </message>
+    <message>
+        <location filename="../keychain_win.cpp" line="65"/>
+        <source>Could not delete encrypted data from settings: format error</source>
+        <translation>Kann verschlüsselte Daten nicht aus den Einstellungen entfernen: Formatfehler</translation>
+    </message>
+    <message>
+        <location filename="../keychain_win.cpp" line="85"/>
+        <source>Encryption failed</source>
+        <translation>Verschlüsselung fehlgeschlagen</translation>
+    </message>
+    <message>
+        <location filename="../keychain_win.cpp" line="100"/>
+        <source>Could not store encrypted data in settings: access error</source>
+        <translation>Kann verschlüsselte Daten nicht in den Einstellungen speichern: Zugriffsfehler</translation>
+    </message>
+    <message>
+        <location filename="../keychain_win.cpp" line="101"/>
+        <source>Could not store encrypted data in settings: format error</source>
+        <translation>Kann verschlüsselte Daten nicht in den Einstellungen speichern: Formatfehler</translation>
+    </message>
+</context>
+</TS>
