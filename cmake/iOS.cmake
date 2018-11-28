ade06d58834708014bea20571920c903f6d0677e
Kolbjörn Gripne <kolbjorn.gripne@qlik.com>
2018-11-28 18:31:55 +0100

    Set min iOS version

--- cmake/iOS.cmake
+++ cmake/iOS.cmake
@@ -36,7 +36,7 @@ set (APPLE True)
 set (IOS True)
 
 # Required as of cmake 2.8.10
-set (CMAKE_OSX_DEPLOYMENT_TARGET "" CACHE STRING "Force unset of the deployment target for iOS" FORCE)
+set (CMAKE_OSX_DEPLOYMENT_TARGET "11.0" CACHE STRING "Set earliest deployment version for iOS" FORCE)
 
 # Determine the cmake host system version so we know where to find the iOS SDKs
 find_program (CMAKE_UNAME uname /bin /usr/bin /usr/local/bin)
