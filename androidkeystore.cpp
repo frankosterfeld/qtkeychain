25d6d2c0a1e9e3e375ca138c7c9192ea085f211e
Frank Osterfeld <frank.osterfeld@kdab.com>
2023-05-12 10:04:35 +0200

    Android: Fix JNI signature for deletion
    Fixes #217

--- androidkeystore.cpp
+++ androidkeystore.cpp
@@ -115,7 +115,7 @@ bool KeyStore::containsAlias(const QString &alias) const
 
 bool KeyStore::deleteEntry(const QString &alias) const
 {
-    callMethod<void>("deleteEntry", "(Ljava/lang/String;)Z", fromString(alias).object());
+    callMethod<void>("deleteEntry", "(Ljava/lang/String;)V", fromString(alias).object());
     return handleExceptions();
 }
 
