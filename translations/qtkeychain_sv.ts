19d78f1bd3e2c871833829447c5248865c9632eb
Daniel Nylander <po@danielnylander.se>
2025-05-30 16:46:02 +0800

    Adding Swedish translation

---
+++ translations/qtkeychain_sv.ts
@@ -0,0 +1,334 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv_SE">
+    <extra-po-header-language>sv_SE</extra-po-header-language>
+    <extra-po-header-language_team></extra-po-header-language_team>
+    <extra-po-header-last_translator>Daniel Nylander &lt;github@danielnylander.se&gt;</extra-po-header-last_translator>
+    <extra-po-header-po_revision_date></extra-po-header-po_revision_date>
+    <extra-po-header-pot_creation_date></extra-po-header-pot_creation_date>
+    <extra-po-header-project_id_version></extra-po-header-project_id_version>
+    <extra-po-header-x_generator>Poedit 3.6</extra-po-header-x_generator>
+    <extra-po-headers>Project-Id-Version,POT-Creation-Date,PO-Revision-Date,Last-Translator,Language-Team,Language,MIME-Version,Content-Type,Content-Transfer-Encoding,Plural-Forms,X-Language,X-Qt-Contexts,X-Generator</extra-po-headers>
+<context>
+    <name>KeyChainClass</name>
+    <message>
+        <location filename="../TestAppExample/keychainclass.cpp" line="23"/>
+        <source>Read key failed: %1</source>
+        <translation>Läsning av nyckel misslyckades: %1</translation>
+    </message>
+    <message>
+        <location filename="../TestAppExample/keychainclass.cpp" line="39"/>
+        <source>Write key failed: %1</source>
+        <translation>Skrivning av nyckel misslyckades: %1</translation>
+    </message>
+    <message>
+        <location filename="../TestAppExample/keychainclass.cpp" line="56"/>
+        <source>Delete key failed: %1</source>
+        <translation>Borttagning av nyckel misslyckades: %1</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::DeletePasswordJobPrivate</name>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="186"/>
+        <source>Could not open keystore</source>
+        <translation>Det gick inte att öppna nyckellagret</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="193"/>
+        <source>Could not remove private key from keystore</source>
+        <translation>Det gick inte att ta bort den privata nyckeln från nyckelagret</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_haiku.cpp" line="168"/>
+        <source>Password not found</source>
+        <translation>Lösenordet hittades inte</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="616"/>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="625"/>
+        <source>Unknown error</source>
+        <translation>Okänt fel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="647"/>
+        <source>Could not open wallet: %1; %2</source>
+        <translation>Det gick inte att öppna plånboken: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="223"/>
+        <source>Password entry not found</source>
+        <translation>Lösenordet hittades inte</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="227"/>
+        <source>Could not decrypt data</source>
+        <translation>Kunde inte avkryptera data</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::JobPrivate</name>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="315"/>
+        <source>Unknown error</source>
+        <translation>Okänt fel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="570"/>
+        <source>Access to keychain denied</source>
+        <translation>Åtkomst till nyckelring nekades</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::PlainTextStore</name>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="73"/>
+        <source>Could not store data in settings: access error</source>
+        <translation>Det gick inte att lagra data i inställningarna: åtkomstfel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="75"/>
+        <source>Could not store data in settings: format error</source>
+        <translation>Det gick inte att lagra data i inställningarna: formatfel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="93"/>
+        <source>Could not delete data from settings: access error</source>
+        <translation>Det gick inte att ta bort data från inställningarna: åtkomstfel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="95"/>
+        <source>Could not delete data from settings: format error</source>
+        <translation>Det gick inte att ta bort data från inställningarna: formatfel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="112"/>
+        <source>Entry not found</source>
+        <translation>Posten hittades inte</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::ReadPasswordJobPrivate</name>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="52"/>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="416"/>
+        <source>Entry not found</source>
+        <translation>Posten hittades inte</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="60"/>
+        <source>Could not open keystore</source>
+        <translation>Det gick inte att öppna nyckellagret</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="69"/>
+        <source>Could not retrieve private key from keystore</source>
+        <translation>Det gick inte att hämta den privata nyckeln från nyckellagret</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="76"/>
+        <source>Could not create decryption cipher</source>
+        <translation>Kunde inte skapa avkrypteringsskiffer</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_haiku.cpp" line="93"/>
+        <source>Password not found</source>
+        <translation>Lösenordet hittades inte</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="219"/>
+        <source>D-Bus is not running</source>
+        <translation>D-Bus är inte igång</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="229"/>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="239"/>
+        <source>Unknown error</source>
+        <translation>Okänt fel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="336"/>
+        <source>No keychain service available</source>
+        <translation>Ingen nyckelringstjänst tillgänglig</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="340"/>
+        <source>Could not open wallet: %1; %2</source>
+        <translation>Det gick inte att öppna plånboken: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="386"/>
+        <source>Access to keychain denied</source>
+        <translation>Åtkomst till nyckelring nekades</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="406"/>
+        <source>Could not determine data type: %1; %2</source>
+        <translation>Datatypen kunde inte fastställas: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="425"/>
+        <source>Unsupported entry type &apos;Map&apos;</source>
+        <translation>Posttypen &apos;Map&apos; stöds inte</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="428"/>
+        <source>Unknown kwallet entry type &apos;%1&apos;</source>
+        <translation>Okänd typ av kwallet-post &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="100"/>
+        <source>Password entry not found</source>
+        <translation>Lösenordsposten hittades inte</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="104"/>
+        <source>Could not decrypt data</source>
+        <translation>Kunde inte avkryptera data</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="126"/>
+        <location filename="../qtkeychain/keychain_win.cpp" line="248"/>
+        <source>Could not decrypt data: %1</source>
+        <translation>Det gick inte att avkryptera data: %1</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::WritePasswordJobPrivate</name>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="96"/>
+        <source>Could not open keystore</source>
+        <translation>Det gick inte att öppna nyckellagret</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="136"/>
+        <source>Could not create private key generator</source>
+        <translation>Det gick inte att skapa en generator för privata nycklar</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="143"/>
+        <source>Could not generate new private key</source>
+        <translation>Det gick inte att generera en ny privat nyckel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="152"/>
+        <source>Could not retrieve private key from keystore</source>
+        <translation>Det gick inte att hämta den privata nyckeln från nyckellagret</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="160"/>
+        <source>Could not create encryption cipher</source>
+        <translation>Det gick inte att skapa krypteringschiffer</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="168"/>
+        <source>Could not encrypt data</source>
+        <translation>Kunde inte kryptera data</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_haiku.cpp" line="137"/>
+        <source>Password not found</source>
+        <translation>Lösenordet hittades inte</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="471"/>
+        <source>D-Bus is not running</source>
+        <translation>D-Bus är inte igång</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="481"/>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="507"/>
+        <source>Unknown error</source>
+        <translation>Okänt fel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="526"/>
+        <source>Could not open wallet: %1; %2</source>
+        <translation>Det gick inte att öppna plånboken: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="159"/>
+        <location filename="../qtkeychain/keychain_win.cpp" line="259"/>
+        <source>Encryption failed: %1</source>
+        <translation>Krypteringen misslyckades: %1</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="164"/>
+        <source>Credential size exceeds maximum size of %1: %2</source>
+        <translation>Autentiseringsstorleken överskrider maxstorleken för %1: %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="206"/>
+        <source>Credential key exceeds maximum size of %1</source>
+        <translation>Autentiseringsnyckeln överskrider den maximala storleken på %1</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="212"/>
+        <source>Writing credentials failed: %1</source>
+        <translation>Skrivning av autentiseringsuppgifter misslyckades: %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../qtkeychain/keychain_haiku.cpp" line="72"/>
+        <source>error 0x%1: %2</source>
+        <translation>fel 0x%1: %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="274"/>
+        <source>Access to keychain denied</source>
+        <translation>Åtkomst till nyckelring nekades</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="276"/>
+        <source>No keyring daemon</source>
+        <translation>Ingen keyring-daemon</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="278"/>
+        <source>Already unlocked</source>
+        <translation>Redan upplåst</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="280"/>
+        <source>No such keyring</source>
+        <translation>Ingen sådan nyckelring</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="282"/>
+        <source>Bad arguments</source>
+        <translation>Felaktiga argument</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="284"/>
+        <source>I/O error</source>
+        <translation>In/ut-fel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="286"/>
+        <source>Cancelled</source>
+        <translation>Avbruten</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="288"/>
+        <source>Keyring already exists</source>
+        <translation>Nyckelringen finns redan</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="290"/>
+        <source>No match</source>
+        <translation>Ingen matchning</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="295"/>
+        <source>Unknown error</source>
+        <translation>Okänt fel</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/libsecret.cpp" line="104"/>
+        <source>Entry not found</source>
+        <translation>Posten hittades inte</translation>
+    </message>
+</context>
+</TS>
