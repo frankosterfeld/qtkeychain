ce876d1e17d9517e30bb9f05f0cb362f7ba6efd6
Kang Lin <kl222@126.com>
2025-10-10 13:34:26 +0800

    Modify TestAppExample to add service

--- TestAppExample/keychainclass.h
+++ TestAppExample/keychainclass.h
@@ -11,6 +11,7 @@ class KeyChainClass : public QObject
 public:
     KeyChainClass(QObject *parent = nullptr);
 
+    Q_INVOKABLE void setService(const QString &service);
     Q_INVOKABLE void readKey(const QString &key);
     Q_INVOKABLE void writeKey(const QString &key, const QString &value);
     Q_INVOKABLE void deleteKey(const QString &key);
@@ -22,9 +23,9 @@ Q_SIGNALS:
     void error(const QString &errorText);
 
 private:
-    QKeychain::ReadPasswordJob m_readCredentialJob;
-    QKeychain::WritePasswordJob m_writeCredentialJob;
-    QKeychain::DeletePasswordJob m_deleteCredentialJob;
+    QKeychain::ReadPasswordJob* m_readCredentialJob;
+    QKeychain::WritePasswordJob* m_writeCredentialJob;
+    QKeychain::DeletePasswordJob* m_deleteCredentialJob;
 };
 
 #endif // KEYCHAINCLASS_H
