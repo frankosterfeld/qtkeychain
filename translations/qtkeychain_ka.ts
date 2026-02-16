047a68cebd257a7aa60ff932200167fbbdf2395d
Ekaterine Papava <papava.e@gtu.ge>
2026-02-16 08:39:11 +0400

    i18n: Add Georgian translation

---
+++ translations/qtkeychain_ka.ts
@@ -0,0 +1,326 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>KeyChainClass</name>
+    <message>
+        <location filename="../TestAppExample/keychainclass.cpp" line="23"></location>
+        <source>Read key failed: %1</source>
+        <translation>გასაღების წაკითხვა ჩავარდა: %1</translation>
+    </message>
+    <message>
+        <location filename="../TestAppExample/keychainclass.cpp" line="39"></location>
+        <source>Write key failed: %1</source>
+        <translation>გასაღების ჩაწერის შეცდომა: %1</translation>
+    </message>
+    <message>
+        <location filename="../TestAppExample/keychainclass.cpp" line="56"></location>
+        <source>Delete key failed: %1</source>
+        <translation>გასაღების წაშლის შეცდომა: %1</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::DeletePasswordJobPrivate</name>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="186"></location>
+        <source>Could not open keystore</source>
+        <translation>გასაღებების საცავის გახსნა შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="193"></location>
+        <source>Could not remove private key from keystore</source>
+        <translation>გასაღებების საცავიდან პირადი გასაღების წაშლა შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_haiku.cpp" line="168"></location>
+        <source>Password not found</source>
+        <translation>პაროლი აღმოჩენილი არაა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="616"></location>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="625"></location>
+        <source>Unknown error</source>
+        <translation>უცნობი შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="647"></location>
+        <source>Could not open wallet: %1; %2</source>
+        <translation>გახსნის შეცდომა საფულისთვის: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="223"></location>
+        <source>Password entry not found</source>
+        <translation>პაროლის ჩანაწერი აღმოჩენილი არაა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="227"></location>
+        <source>Could not decrypt data</source>
+        <translation>მონაცემების გაშიფვრის შეცდომა</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::JobPrivate</name>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="315"></location>
+        <source>Unknown error</source>
+        <translation>უცნობი შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="570"></location>
+        <source>Access to keychain denied</source>
+        <translation>ბრელოკთან წვდომა აკრძალულია</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::PlainTextStore</name>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="73"></location>
+        <source>Could not store data in settings: access error</source>
+        <translation>მონაცემების პარამეტრებში შენახვა შეუძლებელია: წვდომის შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="75"></location>
+        <source>Could not store data in settings: format error</source>
+        <translation>მონაცემების პარამეტრებში შენახვა შეუძლებელია: ფორმატის შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="93"></location>
+        <source>Could not delete data from settings: access error</source>
+        <translation>პარამეტრებიდან მონაცემების წაშლის შეცდომა: წვდომის შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="95"></location>
+        <source>Could not delete data from settings: format error</source>
+        <translation>პარამეტრებიდან მონაცემების წაშლის შეცდომა: ფორმატის შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/plaintextstore.cpp" line="112"></location>
+        <source>Entry not found</source>
+        <translation>ჩანაწერი ვერ ვიპოვე</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::ReadPasswordJobPrivate</name>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="52"></location>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="416"></location>
+        <source>Entry not found</source>
+        <translation>ჩანაწერი ვერ ვიპოვე</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="60"></location>
+        <source>Could not open keystore</source>
+        <translation>გასაღებების საცავის გახსნა შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="69"></location>
+        <source>Could not retrieve private key from keystore</source>
+        <translation>გასაღებების საცავიდან პირადი გასაღების მიღება შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="76"></location>
+        <source>Could not create decryption cipher</source>
+        <translation>გაშიფვრის შიფრის შექმნა შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_haiku.cpp" line="93"></location>
+        <source>Password not found</source>
+        <translation>პაროლი აღმოჩენილი არაა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="219"></location>
+        <source>D-Bus is not running</source>
+        <translation>D-Bus is გაშვებული არაა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="229"></location>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="239"></location>
+        <source>Unknown error</source>
+        <translation>უცნობი შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="336"></location>
+        <source>No keychain service available</source>
+        <translation>ბრელოკი სერვისი ხელმისაწვდომი არაა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="340"></location>
+        <source>Could not open wallet: %1; %2</source>
+        <translation>გახსნის შეცდომა საფულისთვის: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="386"></location>
+        <source>Access to keychain denied</source>
+        <translation>ბრელოკთან წვდომა აკრძალულია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="406"></location>
+        <source>Could not determine data type: %1; %2</source>
+        <translation>მონაცემთა ტიპის დადგენა შეუძლებელია: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="425"></location>
+        <source>Unsupported entry type 'Map'</source>
+        <translation>მხარდაუჭერელი ჩანაწერის ტიპი 'Map'</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="428"></location>
+        <source>Unknown kwallet entry type '%1'</source>
+        <translation>უცნობი kwallet-ის ჩანაწერის ტიპი '%1'</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="100"></location>
+        <source>Password entry not found</source>
+        <translation>პაროლის ჩანაწერი აღმოჩენილი არაა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="104"></location>
+        <source>Could not decrypt data</source>
+        <translation>მონაცემების გაშიფვრის შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="126"></location>
+        <location filename="../qtkeychain/keychain_win.cpp" line="248"></location>
+        <source>Could not decrypt data: %1</source>
+        <translation>მონაცემების გაშიფვრის შეცდომა: %1</translation>
+    </message>
+</context>
+<context>
+    <name>QKeychain::WritePasswordJobPrivate</name>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="96"></location>
+        <source>Could not open keystore</source>
+        <translation>გასაღებების საცავის გახსნა შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="136"></location>
+        <source>Could not create private key generator</source>
+        <translation>პირადი გასაღების გენერატორის შექმნა შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="143"></location>
+        <source>Could not generate new private key</source>
+        <translation>ახალი პირადი გასაღების გენერაცია შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="152"></location>
+        <source>Could not retrieve private key from keystore</source>
+        <translation>გასაღებების საცავიდან პირადი გასაღების მიღება შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="160"></location>
+        <source>Could not create encryption cipher</source>
+        <translation>დაშიფვრის შიფრის შექმნა შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_android.cpp" line="168"></location>
+        <source>Could not encrypt data</source>
+        <translation>მონაცემების დაშიფვრა შეუძლებელია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_haiku.cpp" line="137"></location>
+        <source>Password not found</source>
+        <translation>პაროლი აღმოჩენილი არაა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="471"></location>
+        <source>D-Bus is not running</source>
+        <translation>D-Bus is გაშვებული არაა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="481"></location>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="507"></location>
+        <source>Unknown error</source>
+        <translation>უცნობი შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="526"></location>
+        <source>Could not open wallet: %1; %2</source>
+        <translation>გახსნის შეცდომა საფულისთვის: %1; %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="159"></location>
+        <location filename="../qtkeychain/keychain_win.cpp" line="259"></location>
+        <source>Encryption failed: %1</source>
+        <translation>დაშიფვრის შეცდომა: %1</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="164"></location>
+        <source>Credential size exceeds maximum size of %1: %2</source>
+        <translation>ავტორიზაციის დეტალების ზომა %1-ზე მეტია: %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="206"></location>
+        <source>Credential key exceeds maximum size of %1</source>
+        <translation>ავტორიზაციის დეტალების ზომა %1-ზე მეტია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_win.cpp" line="212"></location>
+        <source>Writing credentials failed: %1</source>
+        <translation>ავტორიზაციის დეტალების ჩაწერა ჩავარდა: %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../qtkeychain/keychain_haiku.cpp" line="72"></location>
+        <source>error 0x%1: %2</source>
+        <translation>შეცდომა 0x%1: %2</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="274"></location>
+        <source>Access to keychain denied</source>
+        <translation>ბრელოკთან წვდომა აკრძალულია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="276"></location>
+        <source>No keyring daemon</source>
+        <translation>ბრელოკის დემონის გარეშე</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="278"></location>
+        <source>Already unlocked</source>
+        <translation>უკვე განბლოკილია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="280"></location>
+        <source>No such keyring</source>
+        <translation>ასეთი ბრელოკი არ არსებობს</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="282"></location>
+        <source>Bad arguments</source>
+        <translation>არასწორი არგუმენტები</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="284"></location>
+        <source>I/O error</source>
+        <translation>I/O შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="286"></location>
+        <source>Cancelled</source>
+        <translation>გაუქმებულია</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="288"></location>
+        <source>Keyring already exists</source>
+        <translation>ბრელოკი უნდა არსებობს</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="290"></location>
+        <source>No match</source>
+        <translation>დამთხვევის გარეშე</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/keychain_unix.cpp" line="295"></location>
+        <source>Unknown error</source>
+        <translation>უცნობი შეცდომა</translation>
+    </message>
+    <message>
+        <location filename="../qtkeychain/libsecret.cpp" line="104"></location>
+        <source>Entry not found</source>
+        <translation>ჩანაწერი ვერ ვიპოვე</translation>
+    </message>
+</context>
+</TS>
